c860deebb8337471477b1d514633be030bf94bce
zhangheng <zhangheng2683@gmail.com>
2016-07-16 22:48:00 +0800

    测试ajax

--- src/main/java/com/zh/web/controller/MyController.java
+++ src/main/java/com/zh/web/controller/MyController.java
@@ -14,9 +14,4 @@ public class MyController {
                         @RequestParam(value = "password", required = false)String jsonData){
         return "Hello," + username + ". Your password is " + password;
     }
-    @RequestMapping("/ajaxJson")
-    @ResponseBody
-    public String login(@RequestParam(value = "jsonData", required = false)String jsonData){
-        return "success";
-    }
 }
